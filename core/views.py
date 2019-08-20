666d6b0ee0d2e3988ac2a701d1b190410247175b
Michael <phamm89@gmail.com>
2019-08-19 23:32:44 -0400

    Setting Up Calculator

--- core/views.py
+++ core/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
+# Views Created Here
+def index(request):
+    """View function for home page of site."""
+    
+    return render(request, 'index.html')
